ad8ca00b14c1e7b59d743e56eec9fc81197b2500
Nuriev Ramazan <ramaketa@gmail.com>
2023-12-01 23:50:25 +0300

    решили 3  задачу

---
+++ task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+
+double[] array = new double[] { 12.0129, 1.120, -2.153, 1765.03248, -29.3918, 47.19238 };
+double max = array[0];
+double min = array[0];
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine($"Минимальное число - {min}");
+Console.WriteLine($"Максимальное число - {max}");
+Console.WriteLine($"разница между максимальным и минимальным элементов массива - {max - min}");
+
+
